e5a0f28ba59e73dd7b455de1607ffd685ee7b6bf
Terrance Niechciol <TNiechciol@gmail.com>
2012-09-07 21:21:24 -0400

    Added second paddle

--- pong.py
+++ pong.py
@@ -15,7 +15,11 @@ PADDLE_HEIGHT = 100
 p1UpPressed = False
 p1DownPressed = False
 
+p2UpPressed = False
+p2DownPressed = False
+
 p1Position = 250
+p2Position = 250
 
 # this is the game loop. since it continues while isRunning is true,
 # setting it to false in the game loop ends the game
@@ -29,21 +33,43 @@ while isRunning:
                 p1UpPressed = True
             elif event.key == pygame.K_s:
                 p1DownPressed = True
+            elif event.key == pygame.K_UP:
+                p2UpPressed = True
+            elif event.key == pygame.K_DOWN:
+                p2DownPressed = True
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_w:
                 p1UpPressed = False
             elif event.key == pygame.K_s:
                 p1DownPressed = False
+            elif event.key == pygame.K_UP:
+                p2UpPressed = False
+            elif event.key == pygame.K_DOWN:
+                p2DownPressed = False
      
     # update code
     if p1UpPressed:
         p1Position -= 5
     if p1DownPressed:
         p1Position += 5
+    if p2UpPressed:
+        p2Position -= 5
+    if p2DownPressed:
+        p2Position += 5
 
     # draw code
+    # clear display
     display.fill((0, 0, 0))
+
+    # draw first player
     pygame.draw.rect(display, (255, 255, 255), (0, p1Position - PADDLE_HEIGHT / 2, PADDLE_WIDTH, PADDLE_HEIGHT))
+
+    #draw second player
+    pygame.draw.rect(display, (255, 255, 255), (500 - PADDLE_WIDTH, p2Position - PADDLE_HEIGHT / 2, PADDLE_WIDTH, PADDLE_HEIGHT))
+
+    # print display to the screen
     pygame.display.flip()
 
+    # delay game so it runs at a specific framerate. If you didn't do this the game would run way too fast,
+    # and would run at a different speed depending on the speed of your computer.
     clock.tick(60)
